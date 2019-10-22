5b59e7cd6e723ee4e13f1a9952dab58b3f8d1ab3
Pathorn <pathorn.teng@gmail.com>
2019-10-22 18:17:52 +0700

    fix memory leakage

--- p2p/conn/connection.go
+++ p2p/conn/connection.go
@@ -813,7 +813,7 @@ func (ch *Channel) recvPacketMsg(packet PacketMsg) ([]byte, error) {
 		// http://stackoverflow.com/questions/16971741/how-do-you-clear-a-slice-in-go
 		//   suggests this could be a memory leak, but we might as well keep the memory for the channel until it closes,
 		//	at which point the recving slice stops being used and should be garbage collected
-		ch.recving = ch.recving[:0] // make([]byte, 0, ch.desc.RecvBufferCapacity)
+		ch.recving = make([]byte, 0, ch.desc.RecvBufferCapacity) // ch.recving[:0] < this one might be the cause of memory leakage
 		return msgBytes, nil
 	}
 	return nil, nil
